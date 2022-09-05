08cd75fe14f513eeade4ed0e3716808642348274
Chuck <miniq123@wemade.com>
2022-09-05 10:59:41 +0900

    [chuck] Sample 수정, curve도 enum으로 설정하도록 extension에 추가

--- Spring/WemadeSpringExtension.swift
+++ Spring/WemadeSpringExtension.swift
@@ -7,10 +7,15 @@
 
 public protocol WemadeSpringExtension where Self: Springable {
     func setAnimation(_ animation: Spring.AnimationPreset)
+    func setCurve(_ curve: Spring.AnimationCurve)
 }
 
 extension WemadeSpringExtension {
     public func setAnimation(_ animation: Spring.AnimationPreset) {
         self.animation = animation.rawValue
+    }
+    
+    public func setCurve(_ curve: Spring.AnimationCurve) {
+        self.curve = curve.rawValue
     }
 }
